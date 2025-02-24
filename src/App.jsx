9315311932f8ccf3047f26668fe9365914dc0d72
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2025-02-24 16:33:39 +0300

    feat: set custom camera settings for Canvas component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import "./App.css";
 
 function App() {
   return (
-    <Canvas>
+    <Canvas
+      camera={{
+        fov: 64,
+        position: [2.3, 1.5, 2.3],
+      }}
+    >
       <Experience />
     </Canvas>
   );
